b67f55e13ade224608542b097e95c51af3d144c8
dupontcam <125046483+dupontcam@users.noreply.github.com>
2023-11-15 11:35:38 -0300

    Salvar Entregador no BD

--- src/main/java/br/uniceub/dianome/controllers/EntregadorController.java
+++ src/main/java/br/uniceub/dianome/controllers/EntregadorController.java
@@ -1,11 +1,14 @@
 package br.uniceub.dianome.controllers;
 
+import br.uniceub.dianome.dtos.EntregadorDto;
 import br.uniceub.dianome.models.EntregadorModel;
 import br.uniceub.dianome.repsitories.EntregadorRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +17,16 @@ public class EntregadorController {
     @Autowired
     EntregadorRepository repository;
 
+    @GetMapping("/entregadores")
     public ResponseEntity<List<EntregadorModel>> listar(){
         List<EntregadorModel> entregadorList = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(entregadorList);
+    }
+
+    @PostMapping("/entregadores")
+    public ResponseEntity<EntregadorModel> salvar(@RequestBody @Valid EntregadorDto dto){
+        var entregadorModel = new EntregadorModel();
+        BeanUtils.copyProperties(dto, entregadorModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(entregadorModel));
     }
 }
