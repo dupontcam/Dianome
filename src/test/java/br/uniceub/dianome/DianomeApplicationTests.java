6a59e2725da3b36f14d1ca5e7f7b54ed5cba8f98
dupontcam <125046483+dupontcam@users.noreply.github.com>
2023-11-15 09:27:56 -0300

    Initial commit

---
+++ src/test/java/br/uniceub/dianome/DianomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uniceub.dianome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
